99e60995880df04e53cc284916c8ddd75e501caf
aabbasi <aabolfazlit@gmail.com>
2021-06-04 14:22:58 +0300

    Add glide library

--- app/src/main/java/me/akay/movies/dependencyInjection/ApplicationModule.kt
+++ app/src/main/java/me/akay/movies/dependencyInjection/ApplicationModule.kt
@@ -1,6 +1,10 @@
 package me.akay.movies.dependencyInjection
 
 import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestManager
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import dagger.Module
 import dagger.Provides
 import me.akay.movies.ApplicationLoader
@@ -28,4 +32,20 @@ class ApplicationModule {
     fun provideContext(application: ApplicationLoader): Context {
         return application.applicationContext
     }
+
+    @Singleton
+    @Provides
+    fun provideRequestOption(): RequestOptions {
+        return RequestOptions.fitCenterTransform()
+            .centerCrop()
+            .apply(RequestOptions.bitmapTransform(RoundedCorners(8)))
+
+    }
+
+    @Singleton
+    @Provides
+    fun provideRequestManager(context: Context, requestOptions: RequestOptions): RequestManager {
+        return Glide.with(context).setDefaultRequestOptions(requestOptions)
+    }
+
 }
